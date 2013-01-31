48abc0f9175a6cb24105864e3d581dc85e19f1d9
Andy @ Office <andybond13@yahoo.com>
2013-01-31 12:24:56 -0500

    Baby steps in getting combiner to work. Got the filesystem reader going!

--- inc/parallelCombiner.h
+++ inc/parallelCombiner.h
@@ -41,10 +41,14 @@ public:
         \param cohPar : cohesive law parameter list, either sigma or delta
     */
 
-	void run(const int procs, const std::string vtkPath);
+	void run(const int procs, const std::string resultsPath);
 
 private:
 
+	std::vector<std::vector<std::string> > _fileSet;
+
+	void findSets(std::string inPath);
+	void combineSet(std::vector<std::string> inSet);
 
 };
 
--- src/parallelCombiner.C
+++ src/parallelCombiner.C
@@ -13,8 +13,13 @@
 *******************************************************************************/
 #include "parallelCombiner.h"
 #include <iostream>
+#include <boost/filesystem.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
+using namespace boost::filesystem; 
 
 /*******************************************************************************
 
@@ -28,9 +33,65 @@ ParallelCombiner::ParallelCombiner ()  {}
 
 ParallelCombiner::~ParallelCombiner ()  {}
 
-void ParallelCombiner::run(const int procs, const std::string vtkPath) {}
+void ParallelCombiner::run(const int procs, const std::string resultsPath) {
+
+	string vtkPath = resultsPath + "/vtkFiles/";
+
+	//check files for sets
+	findSets(vtkPath);
+
+	//combine sets
+	std::vector<std::string> inSet;
+	combineSet(inSet);
+
+}
 
 /*------------------------------ P R I V A T E -------------------------------*/
+
+void ParallelCombiner::findSets(string inPath) {
+
+	string p = inPath;
+	vector<string> allFiles(0);
+
+	//get file listing
+	if (is_directory(p)) {
+		for (directory_iterator itr(p); itr!=directory_iterator(); ++itr) {
+			cout << itr->path().filename() << " "; // display filename only
+			if (is_regular_file(itr->status())) {
+				cout << " [" << file_size(itr->path()) << "]";
+				allFiles.push_back(itr->path().string());
+			}
+			cout << endl;
+		}
+	} else {
+		cout << (exists(p) ? "Found: " : "Not found: ") << p << endl;
+	}
+
+	//check file listing
+	for (unsigned i = 0; i < allFiles.size(); ++i) {
+		cout << allFiles[i] << endl;
+	}
+
+	//sort into sets
+	vector<vector<string> > _fileSet(0);
+	for (unsigned i = 0; i < allFiles.size(); ++i) {
+		vector<string> files(0);
+		files.push_back(allFiles[i]);
+		for (unsigned i = 0; i < allFiles.size(); ++i) {
+			
+	
+		}
+
+		if (files.size() >= 2) _fileSet.push_back(files);
+	}
+	
+
+
+	return;
+}
+
+void ParallelCombiner::combineSet(std::vector<std::string> inSet) {}
+
 
 
 
