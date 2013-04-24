88e8c68e2fafff5f1aa56ddebb30a63a5b8782b2
Andy @ Office <andybond13@yahoo.com>
2013-04-24 14:41:01 -0400

    Tested file - now it's operable! Still need to collect/summarize mass data.

--- automaticRuns.py
+++ automaticRuns.py
@@ -9,38 +9,48 @@ import time
 import os
 
 # Get to the right place
+call(["clear"])
 os.chdir("/Users/andrewstershic/Code/axisymmetricRing/")
 
 # Clean out old results (if needed)
-os.mkdir("results/automatedRuns")
+if not os.path.exists("results/automatedRuns"):
+    os.makedirs("results/automatedRuns")
 filelist = [ f for f in os.listdir("results/automatedRuns") if f.endswith(".dat") ]
 for f in filelist:
-    os.remove(f)
+    os.remove("results/automatedRuns/" + f)
 
 # Compile Program
 call(["make","clean"])
+call(["make"])
 
 # Initialize run series information
-start = time.clock()
-n = 3
+#start = time.clock()
+startT = time.time()
+n = 10
 series = ""
+p = 4
 
 # Run the series
 for x in range(n):
 	print ""
 	print "Execution number %d" % x
-	call(["./bin/main.exe"])
-	print fileName
-	filelist = [ f for f in os.listdir("results/automatedRuns") if f.endswith(".dat") ]
+	if (p == 1):
+		call(["./bin/main.exe"])
+	else:
+		call(["mpirun","-np",str(p),"./bin/main.exe"])
+	filelist = [ f for f in os.listdir("results/datFiles") if f.endswith(".dat") ]
 	for f in filelist:
-		fileName = series + f + str(x) + ".dat"
+		f_raw = f.replace(".dat","")
+		fileName = "results/automatedRuns/" + series + f_raw + "-" + str(x)  + ".dat"
 		print fileName
-		os.rename(f,fileName)
+		os.rename("results/datFiles/"+f,fileName)
 
 # Show timing results
-elapsed = (time.clock() - start)
+#elapsed = (time.clock() - start)
+elapsedT = (time.time() - startT)
 print ""
-print "Average run-time = ",elapsed/n, " seconds"
+#print "Average run-time = ",elapsed/n, " seconds"
+print "Average run-time = ",elapsedT/n, " seconds"
 
 # Need to collect and average data
 # ...
